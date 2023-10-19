cd4de66cb5a16e0c70cf25c12959bf1099f5cefb
PiraTechnics <devin1younge@gmail.com>
2023-10-19 15:20:24 -0700

    #20 responsive grid shop page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ content }) => {
-  return (
-    <div className="container bg-amber-50 min-w-full min-h-screen">
-      <Header />
-      <main className="mt-6 mb-12">{content}</main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="container bg-amber-50 min-w-full min-h-screen flex flex-col">
+			<Header />
+			<main className="mt-6 mb-12 grow">{content}</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,17 @@
 import Layout from "../components/Layout";
 import Item from "../components/Item";
+import { getAllItems } from "../utils/CompendiumApi";
 
 const ShopPage = () => {
+	console.log(getAllItems());
+
 	return (
 		<Layout
 			content={
-				<div className="border-2 p-10 container mx-auto">
-					<p>This is the Shop!</p>
+				<div className="border-2 rounded p-1 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-4">
+					<Item />
+					<Item />
+					<Item />
 					<Item />
 					<Item />
 				</div>
