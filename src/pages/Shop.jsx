94aac6a3987514636028c47189fa85a097df395c
PiraTechnics <devin1younge@gmail.com>
2023-10-23 15:38:02 -0700

    #24 Static Pagination on shopping page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import Layout from "../components/Layout";
 import Item from "../components/Item";
 
@@ -8,6 +9,7 @@ const ShopPage = () => {
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
 
+	//Compendium API data fetch
 	useEffect(() => {
 		fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/all`)
 			.then((response) => {
@@ -26,27 +28,71 @@ const ShopPage = () => {
 
 	if (error) {
 		return <Layout>content={<div>Error! {error.message}</div>}</Layout>;
+	} else if (loading) {
+		return <Layout>content={<div>Loading...</div>}</Layout>;
+	} else {
+		//return our desired content
+		return (
+			<Layout
+				content={<PaginatedItems items={data} itemsPerPage={25} />}
+			></Layout>
+		);
 	}
-
-	return (
-		<Layout
-			content={
-				<div className="border-2 rounded p-1 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-4 text-black">
-					{data &&
-						data.slice(0, 25).map((item) => {
-							/* return <Item key={item.id} entryName={item.name} />; */
-							if (loading) {
-								return <div>still loading!</div>;
-							}
-							return <Item key={item.id} data={item} />;
-						})}
-				</div>
-			}
-		></Layout>
-	);
 };
 
 export default ShopPage;
+
+const Items = ({ currentItems }) => {
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-black">
+			{currentItems &&
+				currentItems.map((item) => {
+					/* return <Item key={item.id} entryName={item.name} />; */
+					return <Item key={item.id} data={item} />;
+				})}
+		</div>
+	);
+};
+
+const PaginatedItems = ({ items, itemsPerPage }) => {
+	const [itemIndex, setItemIndex] = useState(0);
+
+	const endIndex = itemIndex + itemsPerPage;
+	console.log(`loading items from indices ${itemIndex} to ${endIndex}`);
+
+	const currentItems = items.slice(itemIndex, endIndex);
+	const pageCount = Math.ceil(items.length / itemsPerPage);
+
+	const handlePageClick = (event) => {
+		const newIndex = (event.selected * itemsPerPage) % items.length;
+		/* console.log(
+			`User requested page number ${event.selected}, which is offset ${newIndex}. Total number of items: ${items.length}`
+		); */
+		setItemIndex(newIndex);
+	};
+
+	return (
+		<>
+			<div className="container mx-auto flex flex-col items-center">
+				<Items currentItems={currentItems} />
+				<ReactPaginate
+					breakLabel="..."
+					nextLabel="next >"
+					onPageChange={handlePageClick}
+					pageRangeDisplayed={1}
+					pageCount={pageCount}
+					previousLabel="< previous"
+					renderOnZeroPageCount={null}
+					className="mt-4 mb-1 gap-1 flex flex-row font-medium text-sky-500 [&>li>a]:p-1 [&>li>a]:rounded hover:[&>li>a]:text-yellow-200 hover:[&>li>a]:bg-sky-800 focus:[&>li>a]:bg-sky-600 focus:[&>li>a]:text-white md:gap-2 md:[&>li>a]:p-2"
+				/>
+				<div className="text-black">
+					Showing items {itemIndex == 0 ? 1 : itemIndex}-{endIndex} of{" "}
+					{items.length}
+				</div>
+			</div>
+		</>
+	);
+};
 
 //Utility Functions for sort, filter, shuffle
 const shuffleItems = (itemArray) => {
