ecbbb5f388ff9b4fe07685dfa756c849555b082c
PiraTechnics <devin1younge@gmail.com>
2023-10-26 13:05:52 -0700

    #18 Item Unit Tests

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,62 +3,63 @@ import RupeeIcon from "../assets/BotW_Green_Rupee_Icon.png";
 import { useSessionStorage } from "@uidotdev/usehooks";
 
 const Item = ({ data }) => {
-	const [cartItems, setCartItems] = useSessionStorage("cart", []);
-	const [runningTotal, setRunningTotal] = useSessionStorage("total", 0);
+  const [cartItems, setCartItems] = useSessionStorage("cart", []);
+  const [runningTotal, setRunningTotal] = useSessionStorage("total", 0);
 
-	const cartAddHandler = () => {
-		console.log(`Added to cart: 1x ${data.name}, price: ${data.price} rupees`);
-		let prevCart = cartItems;
-		setCartItems([...prevCart, data]);
-		setRunningTotal(runningTotal + data.price);
-	};
+  const cartAddHandler = () => {
+    console.log(`Added to cart: 1x ${data.name}, price: ${data.price} rupees`);
+    let prevCart = cartItems;
+    setCartItems([...prevCart, data]);
+    setRunningTotal(runningTotal + data.price);
+  };
 
-	return (
-		data && (
-			<Card imgAlt="" imgSrc={data.image} className="max-w-sm">
-				<a href="#">
-					<h5 className="text-3xl capitalize font-semibold tracking-tight text-gray-900 dark:text-white">
-						{data.name}
-					</h5>
-				</a>
-				<div className="flex items-center">
-					<span className="mr-1 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800 capitalize">
-						{data.category}
-					</span>
-					<span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-						Available: <span className="text-sm">{data.inventory}</span>
-					</span>
-				</div>
-				<div className=" text-sm font-semibold tracking-tight text-gray-900 dark:text-white">
-					{/* <p className="text-sm">{itemData.description}</p> */}
-					{data.common_locations ? (
-						<p className="mt-2">
-							Ships From: {data.common_locations.join(", ")}
-						</p>
-					) : (
-						<p>Local Pickup Only</p>
-					)}
-				</div>
-				<div className="flex items-center justify-between">
-					<span className="text-2xl font-bold text-gray-900 dark:text-white flex">
-						<img width={40} src={RupeeIcon} alt="rupee icon"></img>
-						<span>{data.price}</span>
-					</span>
-					<button
-						className="rounded-lg bg-sky-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-800 hover:text-yellow-200 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-						type="button"
-						data-drawer-target="shopping-cart-sidebar"
-						data-drawer-show="shopping-cart-sidebar"
-						data-drawer-placement="right"
-						aria-controls="shopping-cart-sidebar"
-						onClick={cartAddHandler}
-					>
-						Add to Cart
-					</button>
-				</div>
-			</Card>
-		)
-	);
+  //NOTE: Add alt text if possible
+  return (
+    data && (
+      <Card imgAlt={data.name} imgSrc={data.image} className="max-w-sm">
+        <a href="#">
+          <h5 className="text-3xl capitalize font-semibold tracking-tight text-gray-900 dark:text-white">
+            {data.name}
+          </h5>
+        </a>
+        <div className="flex items-center">
+          <span className="mr-1 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800 capitalize">
+            {data.category}
+          </span>
+          <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
+            Available: <span className="text-sm">{data.inventory}</span>
+          </span>
+        </div>
+        <div className=" text-sm font-semibold tracking-tight text-gray-900 dark:text-white">
+          {/* <p className="text-sm">{itemData.description}</p> */}
+          {data.common_locations && data.common_locations.length > 0 ? (
+            <p className="mt-2">
+              Ships From: {data.common_locations.join(", ")}
+            </p>
+          ) : (
+            <p>Local Pickup Only</p>
+          )}
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="text-2xl font-bold text-gray-900 dark:text-white flex">
+            <img width={40} src={RupeeIcon} alt="rupee icon"></img>
+            <span>{data.price}</span>
+          </span>
+          <button
+            className="rounded-lg bg-sky-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-800 hover:text-yellow-200 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+            type="button"
+            data-drawer-target="shopping-cart-sidebar"
+            data-drawer-show="shopping-cart-sidebar"
+            data-drawer-placement="right"
+            aria-controls="shopping-cart-sidebar"
+            onClick={cartAddHandler}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </Card>
+    )
+  );
 };
 
 export default Item;
---
+++ tests/item.test.jsx
@@ -0,0 +1,114 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach } from "vitest";
+import Item from "../src/components/Item";
+import { BrowserRouter } from "react-router-dom";
+//import userEvent from "@testing-library/user-event";
+
+let dummyData = {
+  image: "https://picsum.photos/id/237/200/150",
+  name: "Rare Pupper",
+  category: "sweet boi",
+  inventory: 1,
+  price: 30,
+  common_locations: ["California", "Washington"],
+};
+
+/* beforeEach(() => {
+  render(
+    <BrowserRouter>
+      <Item data={dummyData} />
+    </BrowserRouter>
+  );
+}); */
+
+describe("Item Component", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Item data={dummyData} />
+      </BrowserRouter>
+    );
+  });
+
+  it("has an image tagged with its name", () => {
+    expect(
+      screen.getByRole("img", { name: "Rare Pupper" })
+    ).toBeInTheDocument();
+    //screen.debug();
+  });
+
+  it("has a header matching its name", () => {
+    expect(screen.getByRole("heading").textContent).toMatch(/Rare Pupper/i);
+  });
+
+  it("displays its category", () => {
+    expect(screen.getByText("sweet boi")).toBeInTheDocument();
+  });
+
+  it("displays shipping locations", () => {
+    expect(
+      screen.getByText("Ships From: California, Washington")
+    ).toBeInTheDocument();
+  });
+
+  it("displays quantities available", () => {
+    expect(screen.getByText("Available:")).toBeInTheDocument();
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+
+  it("displays the price", () => {});
+
+  it("has an 'add to cart' button", () => {});
+});
+
+describe("Shipping Location(s) display", () => {
+  it("renders a single shipping location without commas", () => {
+    dummyData.common_locations = ["Idaho"];
+    render(
+      <BrowserRouter>
+        <Item data={dummyData} />
+      </BrowserRouter>
+    );
+
+    //screen.debug();
+    expect(screen.getByText("Ships From: Idaho")).toBeInTheDocument();
+  });
+
+  it("renders empty shipping locations as 'Local Pickup Only'", () => {
+    dummyData.common_locations = [];
+    render(
+      <BrowserRouter>
+        <Item data={dummyData} />
+      </BrowserRouter>
+    );
+
+    //screen.debug();
+    expect(screen.getByText("Local Pickup Only")).toBeInTheDocument();
+  });
+
+  it("renders null shipping locations as 'Local Pickup Only'", () => {
+    dummyData.common_locations = null;
+    render(
+      <BrowserRouter>
+        <Item data={dummyData} />
+      </BrowserRouter>
+    );
+
+    //screen.debug();
+    expect(screen.getByText("Local Pickup Only")).toBeInTheDocument();
+  });
+
+  it("renders multiple shipping locations with comma separation", () => {
+    dummyData.common_locations = ["California", "Oregon", "Washington"];
+    render(
+      <BrowserRouter>
+        <Item data={dummyData} />
+      </BrowserRouter>
+    );
+
+    //screen.debug();
+    expect(
+      screen.getByText("Ships From: California, Oregon, Washington")
+    ).toBeInTheDocument();
+  });
+});
