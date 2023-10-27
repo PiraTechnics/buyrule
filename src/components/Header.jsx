84f650aab6acd495d0f248beff6accff2a23c2e8
PiraTechnics <devin1younge@gmail.com>
2023-10-26 19:43:41 -0700

    #30 Dynamic quantity cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 import Brand from "./Brand";
 import { Navbar } from "flowbite-react";
+import CartIcon from "./CartIcon";
+import { useSessionStorage } from "@uidotdev/usehooks";
 
 const Header = () => {
+	const [cartItems, setCartItems] = useSessionStorage("cart", []);
+
 	return (
 		<div className="w-screen bg-sky-600">
 			<Navbar className="bg-sky-600 py-2 mx-auto max-w-7xl ps-8 pe-12 relative flex justify-between">
@@ -26,19 +30,21 @@ const Header = () => {
 							Shopping
 						</Link>
 						{/* drawer init and toggle */}
-						<div className="text-center">
-							<button
-								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-								type="button"
-								data-drawer-target="shopping-cart-sidebar"
-								data-drawer-show="shopping-cart-sidebar"
-								data-drawer-placement="right"
-								//data-drawer-backdrop="false"
-								aria-controls="shopping-cart-sidebar"
-							>
-								Cart
-							</button>
-						</div>
+						<button
+							/* className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" */
+							className="px-1 py-2.5 mr-2 mb-2 flex flex-row hover:text-yellow-200"
+							type="button"
+							data-drawer-target="shopping-cart-sidebar"
+							data-drawer-show="shopping-cart-sidebar"
+							data-drawer-placement="right"
+							//data-drawer-backdrop="false"
+							aria-controls="shopping-cart-sidebar"
+						>
+							<CartIcon />
+							<span class="w-5 h-5 bg-sky-400 rounded-full flex justify-center items-center text-[.7em] font-sans font-semibold">
+								{cartItems.length || 0}
+							</span>
+						</button>
 					</div>
 				</Navbar.Collapse>
 			</Navbar>
